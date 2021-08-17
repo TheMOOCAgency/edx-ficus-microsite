a7d49fd7589ef302695ff3f489550d57e8cdd759
Pprod1 <lucie.ory@themoocagency.com>
2021-08-17 15:47:53 +0200

    update grade script

--- utils/script-grade.py
+++ utils/script-grade.py
@@ -67,7 +67,8 @@ from courseware.user_state_client import DjangoXBlockUserStateClient
 
 
 from datetime import datetime
-from xlwt import *
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 
 from microsite_configuration.models import (
     MicrositeOrganizationMapping,
@@ -89,12 +90,15 @@ except:
 timestr = time.strftime("%Y_%m_%d")
 timesfr = time.strftime("%d.%m.%Y")
 timesfr = str(timesfr)
-wb = Workbook(encoding='utf-8')
-sheet = wb.add_sheet('Rapport')
+wb = Workbook()
+# wb = Workbook(encoding='utf-8')
+sheet = wb.active
+sheet.title= 'Rapport'
 filename = '/home/edxtma/csv/{}_ntp_Engie.xls'.format(timestr)
+
 # Format date
-style1 = XFStyle()
-style1.num_format_str = 'DD/MM/YYYY'
+# date_style = Style(number_format='DD/MM/YYYY')
+# style1.num_format_str = 'DD/MM/YYYY'
 
 # Course_ids must be from same platform : getting microsite via first course_id in list
 course_key = CourseKey.from_string(course_ids[0])
@@ -149,8 +153,7 @@ for course_id in course_ids:
         tma_enrollment,is_exist=TmaCourseEnrollment.objects.get_or_create(course_enrollment_edx=course_enrollments[i])
 
         # Create a new user_data
-        user_data = {}
-        
+        user_data = {}        
 
         # number of connections        
         # connections = 1
@@ -162,7 +165,6 @@ for course_id in course_ids:
         #     pass
         # user_data["connections"] = connections
 
-
         # Update object with user data without grades
         try:
             user_data["first_name"] = json.loads(user.profile.custom_field)['first_name'].capitalize()
@@ -172,16 +174,16 @@ for course_id in course_ids:
             user_data["last_name"] = "n/a"
 
         try:
-            user_data["registration_date"] = datetime.strptime(user.date_joined.strftime('%d/%m/%Y'), '%d/%m/%Y') 
+            user_data["registration_date"] = str(datetime.strptime(user.date_joined.strftime('%d/%m/%Y'), '%d/%m/%Y') )
         except:
             # should not occured
             user_data["registration_date"] = "n/a"    
 
         try:
-            user_data["last_visit"] = datetime.strptime(user.last_login.strftime('%d/%m/%Y'), '%d/%m/%Y')
+            user_data["last_visit"] = str(datetime.strptime(user.last_login.strftime('%d/%m/%Y'), '%d/%m/%Y'))
         except:
             try:
-                user_data["last_visit"] = datetime.strptime(user.date_joined.strftime('%d/%m/%Y'), '%d/%m/%Y')
+                user_data["last_visit"] = str(datetime.strptime(user.date_joined.strftime('%d/%m/%Y'), '%d/%m/%Y'))
             except:
                 # should not occured
                 user_data["last_visit"] = "n/a"
@@ -246,11 +248,8 @@ for course_id in course_ids:
 
 
         for assignment_type_name, subsection_infos in grading_context['all_graded_subsections_by_type'].iteritems():
-            log.info("loop 1")
             for subsection_index, subsection_info in enumerate(subsection_infos, start=1):
-                log.info("loop 2")
                 for scorable_block in subsection_info['scored_descendants']:
-                    log.info("loop 3")
                     header_name = (
                         u"{assignment_type} {subsection_index}: "
                         u"{subsection_name} - {scorable_block_name}"
@@ -277,22 +276,15 @@ for course_id in course_ids:
         # d_o_c_sum = 0
         inverted_score = 0
         answered_total = 0
-
         iteration = 0
 
-        log.info("666666666666666666666")
-        log.info(scorable_block_titles)
-        log.info(scorable_block_titles.items())
 
         for block_location,block_title in scorable_block_titles.items():
             question = {}
            
-
             # Add submit_time
             try:
-                log.info("1")
                 if user_grade.locations_to_scores.get(block_location):
-                    log.info("2")
                     history_entries = list(user_state_client.get_history(user.username, block_location))
                     value = history_entries[0].state.get('student_answers').values()[0]  # ----->  choice_2
 
@@ -300,16 +292,11 @@ for course_id in course_ids:
                     corrected_value = int(corrected_value)
                     corrected_value += 1
                     answer = "choice "+ str(corrected_value)
-                    log.info("3")
                 else:
-                    log.info("4")
-
                     answer=('not graded for student')
             except:
-                log.info("5")
-                answer='inv.'
+                answer='n.a.'
 
-            log.info("6")
             # Add Timestamp
 
             try:
@@ -336,7 +323,7 @@ for course_id in course_ids:
                 else:
                     valueTS=('no time stamp')
             except:
-                valueTS=('inv.')
+                valueTS=('n.a.')
 
             valueTS = str(valueTS)
             # log.info(type(valueTS))
@@ -368,18 +355,17 @@ for course_id in course_ids:
                 0.6 : {"graded":True, "inverted_matrix":-20, "raw_score":0, "score":grade.earned, "choice":answer, "question":block_title, "submit_time":valueTS, "d_o_c": 0.125}
             }
 
-            if grade.earned in degree_table and answer !='inv.': 
+            if grade.earned in degree_table and answer !='n.a.': 
                 question = degree_table[grade.earned]
-
+                inverted_score += question["inverted_matrix"]
+                answered_total += 1
                 # score_sum += degree_table[grade.earned]["score"]
                 # score_sum += str(degree_table[grade.earned]["score"])
                 # score_sum += str(" + ")
                 # d_o_c_sum += degree_table[grade.earned]["d_o_c"]
-                inverted_score += question["inverted_matrix"]
-                answered_total += 1
                 
             else:
-                question = {"graded": False,  "inverted_matrix":0, "raw_score": 0, "score": 0,"choice":"not answered" ,"question":block_title,"submit_time": "n.a.", "d_o_c": 0}
+                question = {"graded": False, "inverted_matrix":0, "raw_score": 0, "score": 0,"choice":"0" ,"question":block_title,"submit_time": "n.a.", "d_o_c": 0}
                 
             questions.append(question)
 
@@ -390,21 +376,11 @@ for course_id in course_ids:
         
         if len(list_question) != 0 :
 
-            # user_data["average_score_raw"] = round(int(raw_score_sum)/len(list_question),2)
             user_data["average_score_raw"] = True
-
-            # user_data["average_score"] = round(int(score_sum)/len(list_question), 2)
             user_data["average_score"] = True
-
-            # user_data["travail"] = round(int(answered_total)/len(list_question), 2)
             user_data["travail"] =  str( (answered_total) / len(list_question) )
-
-            # user_data["average_d_o_c"] = round(int(d_o_c_sum)/len(list_question), 2)
             user_data["average_d_o_c"] = True
-
-            # user_data["centration"] = round(int(raw_score_sum)/len(list_question),2) - round(int(d_o_c_sum)/len(list_question), 2)/100
             user_data["centration"] = True
-
             user_data["average_inverted"] = round(int(inverted_score)/len(list_question), 2)
 
         else :
@@ -418,7 +394,6 @@ for course_id in course_ids:
 
         all_users_data[str(user.id)]= data
         
-
 
 headers = ["Identifiant","Email","Prénom", "Nom de famille","Pays","Entité","Job","Langage","Date d'inscription","Dernière connexion","Temps passé (hh-mm)","Date de soumission (hh-mm)", "Score global brut (en %)","Score global", "Degré de certitude moyen", "Centration" , "Matrice inversée"]
 
@@ -439,24 +414,36 @@ def Getletterfromindex( num):
     return outval
 
 # WRITE FILE
-j = 1
-i = 0
+j = 2
+i = 1
 alphabet = []
 for i, header in enumerate(headers):
-    sheet.write(j, i, header)
+    sheet.cell(j, i+1, header)
 
-i=17
+i=18
+color = "00D0D0D0"
+
 for question in list_question:
-    # sheet.write(0, i, "bonne réponse")
-    # sheet.write(0, i+1, "titre cours")
-    sheet.write(0, i, question)
-    # sheet.write(0, i+3, "titre sous-chapitre")
-    sheet.write(j, i, "Réponse étudiant")
-    sheet.write(j, i+1, "Note brute")
-    sheet.write(j, i+2, "Degré de certitude")
-    sheet.write(j, i+3, "Score final")
-    sheet.write(j, i+4, "Soumission")
+    # sheet.cell(0, i, "bonne réponse")
+    # sheet.cell(0, i+1, "titre cours")
+    sheet.cell(1, i, question).fill = PatternFill('solid', fgColor=color)
+    sheet.cell(1, i+1).fill = PatternFill('solid', fgColor=color)
+    sheet.cell(1, i+2).fill = PatternFill('solid', fgColor=color)
+    sheet.cell(1, i+3).fill = PatternFill('solid', fgColor=color)
+    sheet.cell(1, i+4).fill = PatternFill('solid', fgColor=color)
+    sheet.cell(j, i, "Réponse étudiant").fill = PatternFill('solid', fgColor=color)
+    sheet.cell(j, i+1, "Note brute").fill = PatternFill('solid', fgColor=color)
+    sheet.cell(j, i+2, "Degré de certitude").fill = PatternFill('solid', fgColor=color)
+    sheet.cell(j, i+3, "Score final").fill = PatternFill('solid', fgColor=color)
+    sheet.cell(j, i+4, "Soumission").fill = PatternFill('solid', fgColor=color)
+
     i += 5 
+    if color == "00D0D0D0" :
+        color = "00FFFFFF"
+    else:
+        color = "00D0D0D0"
+
+
 
 j = 2
 for index, user in all_users_data.items():
@@ -465,77 +452,88 @@ for index, user in all_users_data.items():
     score_sum = ""
     d_o_c_sum = ""
     
-    sheet.write(j, 0, user["general"]["id"])
-    sheet.write(j, 1, user["general"]["email_address"])
-    sheet.write(j, 2, user["general"]["first_name"])
-    sheet.write(j, 3, user["general"]["last_name"])
-    sheet.write(j, 4, user["general"]["country"])
-    sheet.write(j, 5, user["general"]["entity"])
-    sheet.write(j, 6, user["general"]["job"])
-    sheet.write(j, 7, user["general"]["langage"])
+    sheet.cell(j+1, 1, user["general"]["id"])
+    sheet.cell(j+1, 2, user["general"]["email_address"])
+    sheet.cell(j+1, 3, user["general"]["first_name"])
+    sheet.cell(j+1, 4, user["general"]["last_name"])
+    sheet.cell(j+1, 5, user["general"]["country"])
+    sheet.cell(j+1, 6, user["general"]["entity"])
+    sheet.cell(j+1, 7, user["general"]["job"])
+    sheet.cell(j+1, 8, user["general"]["langage"])
     try:
-        sheet.write(j, 8, user["general"]["registration_date"], style1)
+        sheet.cell(j+1, 9, user["general"]["registration_date"])
+        # cell.number_format = 'YYYY MMM DD'
     except:
-        sheet.write(j, 8, "n.a.")
+        sheet.cell(j+1, 9, "n.a.")
 
     try:
-        sheet.write(j, 9, user["general"]["last_visit"], style1)
+        sheet.cell(j+1, 10, user["general"]["last_visit"])
+        # sheet.cell(j+1, 9, user["general"]["last_visit"], style1)
     except:
-        sheet.write(j, 9, "n.a.")
+        sheet.cell(j+1, 10, "n.a.")
 
-    sheet.write(j, 10, user["general"]["time_tracking"])
+    sheet.cell(j+1, 11, user["general"]["time_tracking"])
     try:
-        sheet.write(j, 11, user["general"]["last_submit"], style1)
+        sheet.cell(j+1, 12, user["general"]["last_submit"])
+        # sheet.cell(j+1, 12, user["general"]["last_submit"], style1)
     except:
-        sheet.write(j, 11, 'no time stamp')
+        sheet.cell(j+1, 12, 'no time stamp')
 
-    i=17
+    i=18
+    color = "00D0D0D0"
     for grade in user["grades"]:
-        sheet.write(j, i, grade["choice"])
-        sheet.write(j, i+1, grade["raw_score"])
-        sheet.write(j, i+2, grade["d_o_c"])
-        sheet.write(j, i+3, grade["score"])
-        sheet.write(j, i+4, grade["submit_time"])
 
-        raw_score_sum += Getletterfromindex(i+2) + str(j+1)
+
+        sheet.cell(j+1, i, grade["choice"]).fill = PatternFill('solid', fgColor=color)
+        sheet.cell(j+1, i+1, grade["raw_score"]).fill = PatternFill('solid', fgColor=color)
+        sheet.cell(j+1, i+2, grade["d_o_c"]).fill = PatternFill('solid', fgColor=color)
+        sheet.cell(j+1, i+3, grade["score"]).fill = PatternFill('solid', fgColor=color)
+        sheet.cell(j+1, i+4, grade["submit_time"]).fill = PatternFill('solid', fgColor=color)
+
+        raw_score_sum += Getletterfromindex(i+1) + str(j+1)
         raw_score_sum += " + "
-        d_o_c_sum += Getletterfromindex(i+3) + str(j+1)
+        d_o_c_sum += Getletterfromindex(i+2) + str(j+1)
         d_o_c_sum += " + "
-        score_sum += Getletterfromindex(i+4) + str(j+1)
+        score_sum += Getletterfromindex(i+3) + str(j+1)
         score_sum += " + "
 
         i += 5
+
+        if color == "00D0D0D0" :
+            color = "00FFFFFF"
+        else:
+            color = "00D0D0D0"
+
 
     raw_score_sum = raw_score_sum[0:-2]
     d_o_c_sum = d_o_c_sum[0:-2]
     score_sum = score_sum[0:-2]
 
     if user["general"]["average_score_raw"] : 
-        # sheet.write(j, 12, Formula(user["general"]["average_score_raw"]))
-        sheet.write(j, 12, Formula("("+raw_score_sum + ")*100 /" + str(len(user["grades"])) ))
+        sheet.cell(j+1, 13, ("=SUM("+raw_score_sum+")*100 /" + str(len(user["grades"])) ))
     else:
-        sheet.write(j, 12, "n.a.")
+        sheet.cell(j+1, 13, "n.a.")
     
     if user["general"]["average_score"] :
-        sheet.write(j, 13, Formula("("+score_sum + ")*100 /" + str(len(user["grades"]))) )   
+        sheet.cell(j+1, 14, ("=SUM("+score_sum +")*100 /" + str(len(user["grades"]))) )   
     else:
-        sheet.write(j, 13, "n.a.")   
+        sheet.cell(j+1, 14, "n.a.")   
 
     if user["general"]["average_d_o_c"] :
-        sheet.write(j, 14, Formula("("+d_o_c_sum + ")*100 /" + str(len(user["grades"]))))
+        sheet.cell(j+1, 15, ("=SUM("+d_o_c_sum + ")*100 /" + str(len(user["grades"]))))
     else: 
-        sheet.write(j, 14, "n.a.")
+        sheet.cell(j+1, 15, "n.a.")
 
     if user["general"]["centration"] :
         # La centration est la différence entre le taux de bonne réponse (Nb de bonnes réponses/Nb de questions) et la certitude moyenne (degrée de certitude moyen. On prend comme référence pour une plage de certitude, la médiane. Par exemple le degrée de certitude 70%-85% deviens 77,5%
-        sheet.write(j, 15, Formula((Getletterfromindex(13) + str(j+1)) +" - "+ (Getletterfromindex(15) + str(j+1)) ))
+        sheet.cell(j+1, 16, ("=SUM("+(Getletterfromindex(13)+str(j+1))+"-"+(Getletterfromindex(15)+str(j+1))+")"))
     else :
-        sheet.write(j, 15, "n.a.")
+        sheet.cell(j+1, 16, "n.a.")
 
     if user["general"]["average_inverted"] :
-        sheet.write(j, 16, user["general"]["average_inverted"])
+        sheet.cell(j+1, 17, user["general"]["average_inverted"])
     else :
-        sheet.write(j, 16, "n.a.")
+        sheet.cell(j+1, 17, "n.a.")
 
     j += 1
 
