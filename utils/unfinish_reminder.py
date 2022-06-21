5256b9b540fae141c4ec4edc7456299c221cb60a
Pprod1 <lucie.ory@themoocagency.com>
2022-06-21 16:27:04 +0200

    mail reminder forunfinished courses

--- utils/unfinish_reminder.py
+++ utils/unfinish_reminder.py
@@ -45,6 +45,7 @@ from django.conf import settings
 from pprint import pformat
 from opaque_keys.edx.keys import CourseKey
 from student.views import password_reset_confirm_wrapper
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from lms.djangoapps.grades.new.course_grade import CourseGradeFactory
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from student.models import CourseEnrollment, UserProfile
@@ -65,27 +66,37 @@ for course_dict in courses_dict:
     course_id=course_dict['id']
     course_key=CourseKey.from_string(course_id)
     course=get_course_by_id(course_key)
+    days_allowed = configuration_helpers.get_value_for_org(org, 'COURSES_REGISTRATION_PERIOD')
+    try: 
+        days_allowed_for_course = days_allowed[course_id]
+    except:
+        days_allowed_for_course = None
 
     #get course enrolls
     course_enrollments=CourseEnrollment.objects.filter(course_id=course_key)
 
     #get users that has never been connected
-    log.info('[WUL] : {} users in {}'.format(len(course_enrollments),course_enrollments[0].course_id))  
-    for course_enrollment in course_enrollments:
-        user=course_enrollment.user
-        try:
+    log.info('[WUL] : {} users in {}'.format(len(course_enrollments),course_enrollments[0].course_id))
+    if days_allowed_for_course is not None:
+        for course_enrollment in course_enrollments:
+            user=course_enrollment.user
             user_profile = UserProfile.objects.get(user=user)
             custom_field = json.loads(user_profile.custom_field)
             start_course_ts=int(custom_field.get(course_id, False))/1000
-            if start_course_ts:
+
+            if start_course_ts > 0:
                 start_course_dt=datetime.fromtimestamp(start_course_ts).date()
-                if start_course_dt == (today - timedelta(days=15)):
+                days_passed = today - start_course_dt
+                days_passed_int = int(days_passed.days)
+                remaining_days = days_allowed_for_course - days_passed_int
+
+                if remaining_days == 15 or remaining_days == 5:
                     course_grade = CourseGradeFactory().create(user, course)
                     if not course_grade.passed:
-                        user_info={'user':user,'course_id':course_id,'course_name':course_name}
+                        log.info(user)
+                        user_info={'user':user,'course_id':course_id,'course_name':course_name, 'remaining_days':remaining_days}
                         users_infos.append(user_info)
-        except:
-            pass
+
 
 if len(users_infos) >= 1:
     # SEND MAILS
@@ -93,19 +104,20 @@ if len(users_infos) >= 1:
         user=users_infos[i]['user']
         course_id=users_infos[i]['course_id']
         course_name=users_infos[i]['course_name']
+        remaining_days=users_infos[i]['remaining_days']
         uid=int_to_base36(user.id)
         token = default_token_generator.make_token(user)
         reset_password_link = str(reverse(password_reset_confirm_wrapper, args=(uid, token)))
 
-        html = "<html><head></head><body><p>Bonjour,<br /><br />Cela fait 15 jours que vous avez démarré le cours {course_name}, il vous reste encore 15 jours pour finaliser cours avant que vos accès ne soient bloqués.<br />Connectez-vous dès maintenant à l'adresse ci-dessous pour poursuivre votre progression :<br /><a href=https://www.e-formation.artisanat.fr/dashboard/{course_id}>https://www.e-formation.artisanat.fr/dashboard/{course_id}</a><br /><br />Si vous avez des difficultés à vous connecter vous pouvez réinitialiser votre mot de passe avec ce lien :<br /><a href=https://www.e-formation.artisanat.fr/{reset_password_link}>https://www.e-formation.artisanat.fr/{reset_password_link}</a><br /><br />Cordialement,<br />L'équipe e-formation-artisanat</p></body></html>".format(course_name=course_name,course_id=course_id,reset_password_link=reset_password_link)
+        html = "<html><head></head><body><p>Bonjour,<br /><br />Il ne vous reste plus que {remaining_days} jours pour finaliser la formation {course_name}.<br />Vous pouvez reprendre dès maintenant votre progression pour finaliser le cours et débloquer l'attestation de réussite :<br /><a href=https://academie-digitale.the-mooc-agency.com/dashboard/{course_id}>https://academie-digitale.the-mooc-agency.com/dashboard/{course_id}</a><br /><br />N’hésitez pas à nous écrire à l’adresse supportdigital@cma-france.fr si vous rencontrez des difficultés pour suivre votre formation.<br /><br />Le réseau des Chambres de Métiers et de l’Artisanat vous souhaite une excellente formation !<br /><br />Sincèrement,<br />L’équipe d’accompagnement des formations digitales du réseau des CMA</p></body></html>".format(remaining_days=remaining_days,course_name=course_name,course_id=course_id)
         part2 = MIMEText(html.encode('utf-8'), 'html', 'utf-8')
 
-        fromaddr = "e-formation-artisanat <ne-pas-repondre@themoocagency.com>"
+        fromaddr = "academie-digitale <ne-pas-repondre@themoocagency.com>"
         toaddr = str(user.email)
         msg = MIMEMultipart()
         msg['From'] = fromaddr
         msg['To'] = toaddr
-        msg['Subject'] = "Votre accès au MOOC {} se termine bientôt !".format(course_name)
+        msg['Subject'] = "[academie-digitale] Ne vous arrêtez pas en si bon chemin !"
         part = MIMEBase('application', 'octet-stream')
         server = smtplib.SMTP('mail3.themoocagency.com', 25)
         server.starttls()
