6d1dabd443c86a7f6671bbbadf3604d96f47b210
TMA <sysadmin@themoocagency.com>
2023-07-13 10:58:07 +0200

    17614 - add multiple course feature

--- utils/report_script.py
+++ utils/report_script.py
@@ -46,14 +46,21 @@ import logging
 log = logging.getLogger()
 
 
+# ARGS
 string_emails = sys.argv[1]
 TO_EMAILS = string_emails.split(';')
 
-course_id = sys.argv[2]
-course_key = CourseKey.from_string(course_id)
-course = get_course_by_id(course_key)
+course_id_list = sys.argv[2].split(';')
+
+
+course_id = course_id_list[0]
+fragmented_course_id = course_id.split('+')
+lang = fragmented_course_id[1]
+
 
 #get microsite
+course_key = CourseKey.from_string(course_id_list[0])
+course = get_course_by_id(course_key)
 org = course.org
 query = "SELECT a.id,a.organization,b.key FROM microsite_configuration_micrositeorganizationmapping a,microsite_configuration_microsite b WHERE a.microsite_id = b.id"
 microsite_list = MicrositeOrganizationMapping.objects.raw(query)
@@ -64,15 +71,16 @@ for row in microsite_list:
 
 
 timestr = time.strftime("%Y_%m_%d")
-fragmented_course_id = course_id.split('+')
-lang = fragmented_course_id[1]
+
+
+
+course_enrollment = []
 
 #get course enrolls
-course_enrollment = CourseEnrollment.objects.filter(course_id=course_key)
 
 #grades reports summary
-user_repports_summary=course_enrollment[0]
-user_summary=user_repports_summary.user
+# user_repports_summary=course_enrollment[0]
+# user_summary=user_repports_summary.user
 
 wb = Workbook()
 filename = '/home/edxtma/csv/{}_LVMH_report_{}.xlsx'.format(timestr, lang)
@@ -96,99 +104,109 @@ for i, header in enumerate(HEADERS):
 #NOW LOOK AT GRADES
 j=1
 
-for i in range(len(course_enrollment)):
-    enrollment = course_enrollment[i]
 
-    user = course_enrollment[i].user
-    email = user.email
+for course_id in course_id_list :
 
-    tma_enrollment, is_exist = TmaCourseEnrollment.objects.get_or_create(course_enrollment_edx=enrollment)
-    time_tracking = convert_second_to_time(tma_enrollment.global_time_tracking)
+    course_key = CourseKey.from_string(course_id)
+    course = get_course_by_id(course_key)
 
-    if (email.find('@weuplearning') != -1 or email.find('@yopmail') != -1 or email.find('@themoocagency.com') != -1) :
-        continue
+    course_enrollment = CourseEnrollment.objects.filter(course_id=course_key)
 
-    last_login = user.last_login
-    if last_login :
-        j=j+1
 
-        username=user.username
-        user_profile = json.loads(UserProfile.objects.get(user=user).custom_field)
+    for i in range(len(course_enrollment)):
 
-        try:
-            first_name = user.first_name
-        except:
-            try: 
-                first_name = user_profile['first_name']
+        enrollment = course_enrollment[i]
+
+        user = course_enrollment[i].user
+        email = user.email
+
+        tma_enrollment, is_exist = TmaCourseEnrollment.objects.get_or_create(course_enrollment_edx=enrollment)
+        time_tracking = convert_second_to_time(tma_enrollment.global_time_tracking)
+
+        if (email.find('@weuplearning') != -1 or email.find('@yopmail') != -1 or email.find('@themoocagency.com') != -1) :
+            continue
+
+        last_login = user.last_login
+        if last_login :
+            j=j+1
+
+            username=user.username
+            user_profile = json.loads(UserProfile.objects.get(user=user).custom_field)
+
+            try:
+                first_name = user.first_name
             except:
-                first_name = ""
+                try: 
+                    first_name = user_profile['first_name']
+                except:
+                    first_name = ""
 
-        try:
-            last_name = user.last_name
-        except:
-            try: 
-                last_name = user_profile['last_name']
+            try:
+                last_name = user.last_name
             except:
-                last_name = ""
+                try: 
+                    last_name = user_profile['last_name']
+                except:
+                    last_name = ""
 
-        try:
-            user_cohorte = get_cohort(user, course_key)
-            cohort_name = user_cohorte.name
-        except: 
-            cohort_name = 'Groupe par défaut'
+            try:
+                user_cohorte = get_cohort(user, course_key)
+                cohort_name = user_cohorte.name
+            except: 
+                cohort_name = 'Groupe par défaut'
 
 
-        date_inscription = user.date_joined.strftime('%d/%m/%Y')
-        last_login=user.last_login.strftime('%d/%m/%Y')
-        client = email.split('@')[1]
+            date_inscription = user.date_joined.strftime('%d/%m/%Y')
+            last_login=user.last_login.strftime('%d/%m/%Y')
+            client = email.split('@')[1]
 
-        primary_rows = [
-            email,
-            first_name,
-            last_name,
-            date_inscription, 
-            last_login,
-            time_tracking
-        ]
+            primary_rows = [
+                email,
+                first_name,
+                last_name,
+                date_inscription, 
+                last_login,
+                time_tracking
+            ]
 
-        last_rows = [
-            client,
-            cohort_name
-        ]
+            last_rows = [
+                client,
+                cohort_name
+            ]
 
-        # Start to cell the line
-        l=1
+            # Start to cell the line
+            l=1
 
-        for prim_row in primary_rows:
-            sheet.cell(j, l, prim_row)
+            for prim_row in primary_rows:
+                sheet.cell(j, l, prim_row)
+                l=l+1
+
+
+            # -> quiz ptincipal + quiz alternatif mais une seule note, on regarde donc percent qui est la note globale
+            course_grade = CourseGradeFactory().create(user, course)
+
+            # Bug with percent over 100%
+            percent = str(course_grade.percent * 100) + '%'
+            if course_grade.percent > 1 :
+                percent = str(course_grade.percent * 10) + '%'
+
+            sheet.cell(j, l, percent.replace('.',','))
             l=l+1
 
+            if int(percent.split('.')[0]) >= 75 : 
+                sheet.cell(j, l, 'COMPLETED')
+                sheet.cell(j, l).fill = PatternFill("solid", fgColor="008000")
+                sheet.cell(j, l).font = Font(b=False, color="FFFFFF")
+            else:
+                sheet.cell(j, l, 'NOT COMPLETED')
+                sheet.cell(j, l).fill = PatternFill("solid", fgColor="FF0000")
+                sheet.cell(j, l).font = Font(b=False, color="FFFFFF")
 
-        # -> quiz ptincipal + quiz alternatif mais une seule note, on regarde donc percent qui est la note globale
-        course_grade = CourseGradeFactory().create(user, course)
-
-        # Bug with percent over 100%
-        percent = str(course_grade.percent * 100) + '%'
-        if course_grade.percent > 1 :
-            percent = str(course_grade.percent * 10) + '%'
-
-        sheet.cell(j, l, percent.replace('.',','))
-        l=l+1
-
-        if int(percent.split('.')[0]) >= 75 : 
-            sheet.cell(j, l, 'COMPLETED')
-            sheet.cell(j, l).fill = PatternFill("solid", fgColor="008000")
-            sheet.cell(j, l).font = Font(b=False, color="FFFFFF")
-        else:
-            sheet.cell(j, l, 'NOT COMPLETED')
-            sheet.cell(j, l).fill = PatternFill("solid", fgColor="FF0000")
-            sheet.cell(j, l).font = Font(b=False, color="FFFFFF")
-
-        l=l+1
-
-        for last_row in last_rows:
-            sheet.cell(j, l, last_row)
             l=l+1
+
+            for last_row in last_rows:
+                sheet.cell(j, l, last_row)
+                l=l+1
 
 
 output = BytesIO()
@@ -221,7 +239,7 @@ for i in range(len(TO_EMAILS)):
    print('mail send to '+str(TO_EMAILS[i]))
 
 # TEST 
-# sudo -H -u edxapp /edx/bin/python.edxapp /edx/app/edxapp/edx-microsite/lvmh-elearninginternalcontrol/utils/report_script.py "cyril.adolf@weuplearning.com" course-v1:lvmh-elearninginternalcontrol+EN+2022 
+# sudo -H -u edxapp /edx/bin/python.edxapp /edx/app/edxapp/edx-microsite/lvmh-elearninginternalcontrol/utils/report_script.py "cyril.adolf@weuplearning.com" "course-v1:lvmh-elearninginternalcontrol+FR+2021;course-v1:lvmh-elearninginternalcontrol+FR+2022"
 
 # CRONTAB
 # 0 7 * * MON sudo -H -u edxapp /edx/bin/python.edxapp /edx/app/edxapp/edx-microsite/lvmh-elearninginternalcontrol/utils/report_script.py "emma.bekaert@lvmh.com;maico.naidedasilva@lvmh.com;manal.touati@weuplearning.com" course-v1:lvmh-elearninginternalcontrol+EN+2022 
